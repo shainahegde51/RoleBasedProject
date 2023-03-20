5d96b1a83bd2b925d7c6cdf7c2fcf134f2d997af
Shaina Hegde <shainahegde51@gmail.com>
2023-03-20 12:19:05 +0530

    UserService implementation

---
+++ src/main/java/com/example/RoleBasedProject/Service/CustomUserDetails.java
@@ -0,0 +1,62 @@
+package com.example.RoleBasedProject.Service;
+
+import com.example.RoleBasedProject.Entity.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+
+import java.util.Collection;
+import java.util.Collections;
+
+
+//Currently we are concentrating on username,password,role .so I'm setting rest everything to true
+
+@Component
+public class CustomUserDetails implements UserDetails {
+
+
+    private User user;
+
+    public CustomUserDetails(User user) {
+        super();
+        this.user=user;
+    }
+
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return Collections.singleton(new SimpleGrantedAuthority(user.getRole()));
+    }
+
+    @Override
+    public String getPassword() {
+        return user.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getUserName();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
--- src/main/java/com/example/RoleBasedProject/Service/UserDetailsServiceImpl.java
+++ src/main/java/com/example/RoleBasedProject/Service/UserDetailsServiceImpl.java
@@ -1,17 +1,34 @@
 package com.example.RoleBasedProject.Service;
 
+import com.example.RoleBasedProject.Entity.User;
+import com.example.RoleBasedProject.Repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Repository;
+import org.springframework.stereotype.Service;
 
+@Service
 public class UserDetailsServiceImpl implements UserDetailsService {
 
     //This method is all about loading the user with his/her username ,
     // where we need to include one function called findByUsername in repository
     // which will help this function to load the username
+    @Autowired
+    private UserRepository userRepository;
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return null;
+        User user=userRepository.findByUsername(username);
+        if (user == null) {
+            throw new UsernameNotFoundException("User Not Found");
+        }
+        //we have to pass data back as UserDetails ,So we have to implement UserDetails as well
+        //Inside this UserDetails we can leverage this particular user that we are getting
+
+
+        return new CustomUserDetails(user);
+//
     }
 }
