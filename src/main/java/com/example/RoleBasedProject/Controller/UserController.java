b344864e48616b47b52bfa1eaa2aa76c3538a43e
Shaina Hegde <shainahegde51@gmail.com>
2023-03-20 11:08:57 +0530

    Created the user controller and added the required apis without the implementation

---
+++ src/main/java/com/example/RoleBasedProject/Controller/UserController.java
@@ -0,0 +1,33 @@
+package com.example.RoleBasedProject.Controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class UserController {
+
+
+    //This api can only be used by user
+    @GetMapping({"/user"})
+    public String home(){
+        return "Hi welcome to the Home Page";
+    }
+
+
+    //This api can be used only by admin
+    @GetMapping({"/admin"})
+    public String admin(){
+        return "Hi welcome to the Admin Page";
+    }
+
+
+    //Its a public api where both user and admin can use it
+    @PostMapping({"/addUser"})
+    public String addUser(){
+        return  "New User Added";
+    }
+}
