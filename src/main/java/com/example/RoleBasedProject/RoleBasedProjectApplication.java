f54c0b39e89f439390ebe767e9c04508b0862863
Shaina Hegde <shainahegde51@gmail.com>
2023-03-20 10:40:33 +0530

    Initial Commit -Git Connection

---
+++ src/main/java/com/example/RoleBasedProject/RoleBasedProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.RoleBasedProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoleBasedProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoleBasedProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/RoleBasedProject/RoleBasedProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.RoleBasedProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoleBasedProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
